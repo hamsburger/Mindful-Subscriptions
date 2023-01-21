cd38fae1f17107a5d5dd94a528f682cada13b5d1
hamsburger <harriszheng222@gmail.com>
2023-01-21 14:10:46 -0500

    Subscriptions yeah

--- react-boilerplate-material/src/features/Subscriptions/Subscriptions.tsx
+++ react-boilerplate-material/src/features/Subscriptions/Subscriptions.tsx
@@ -1,6 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useDispatch, useStore } from 'react-redux';
+import { useHistory } from 'react-router-dom';
+import { ISubActionTypes, ISubActionCreator, ISubState } from 
+'models/ISubscriptions';
+
 
 function Subscriptions(){
+    const store = useStore();
+    const dispatch = useDispatch();
+
+    // dispatch({ type : ISubActionTypes.ADD});
     return (
         <div>
          "Subscriptions"   
--- react-boilerplate-material/src/stores/rootReducers.ts
+++ react-boilerplate-material/src/stores/rootReducers.ts
@@ -3,10 +3,11 @@ import { combineReducers } from 'redux';
 // reducers
 import app from 'reducers/app.reducer';
 import auth from 'reducers/auth.reducer';
-
+import subscriptions from 'reducers/subscriptions.reducer'
 const reducers = combineReducers({
   app,
   auth,
+  subscriptions
 });
 
 export default reducers;
